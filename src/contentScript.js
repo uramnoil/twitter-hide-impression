739e24fd26b681c07ee139e19f00f3b195f31382
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2022-12-23 11:00:05 +0000

    :art: リファクタリング

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,31 +2,28 @@
 
 // インプレッションを非表示にする
 const hideAnalytics = () => {
-  var analyticses = document.querySelectorAll("a[href$='/analytics']");
+  const analyticses = document.querySelectorAll("a[href$='/analytics']");
   analyticses.forEach(analytics => analytics.remove());
 }
 
 // ロード完了時のイベントハンドラ
 const onLoaded = () => {
-  // 
-  const jsInitCheckTimer = setInterval(jsLoaded, 1000);
-  // コンテンツのロード完了時のイベントハンドラ
-  function jsLoaded() {
+  const intervalId = setInterval(function() {
     // mainタグがロードされたらintervalを終了する
-    if (document.querySelector("main") != null) {
-      clearInterval(jsInitCheckTimer);
+    if (document.querySelector("a[href$='/analytics']") != null) {
+      clearInterval(intervalId);
+      hideAnalytics()
 
       const target = document.getElementsByTagName('main')[0];
       //オブザーバーの作成
-      var observer = new MutationObserver(hideAnalytics);
+      const observer = new MutationObserver(hideAnalytics);
       const config = {attributes: true, childList: true, subtree: true};
       //監視の開始
       observer.observe(target, config);
-      hideAnalytics()
     } else {
       console.log("waiting...")
     }
-  }
-} 
+  }, 1000);
+}
 
 window.addEventListener('load', onLoaded, false); 
