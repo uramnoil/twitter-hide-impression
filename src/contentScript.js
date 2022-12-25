25b58bdd149acd5f1600a1a0cc4cc4ecdc3953eb
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2022-12-25 21:28:00 +0900

    :memo: add TODO

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,6 +2,7 @@
 
 // インプレッションを非表示にする
 const hideAnalytics = () => {
+  // TODO: インプレッションの子孫要素を削除しており表示が崩れているので、一番外側のタグ自体を消したい
   const analyticses = document.querySelectorAll("a[href$='/analytics']");
   analyticses.forEach(analytics => analytics.remove());
 }
@@ -15,10 +16,10 @@ const onLoaded = () => {
       hideAnalytics()
 
       const target = document.getElementsByTagName('section')[0];
-      //オブザーバーの作成
+      // オブザーバーの作成
       const observer = new MutationObserver(hideAnalytics);
       const config = {attributes: true, childList: true, subtree: true};
-      //監視の開始
+      // 監視の開始
       observer.observe(target, config);
     } else {
       console.log("waiting...")
