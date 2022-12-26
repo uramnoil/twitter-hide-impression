1e013ad8e16c34db9d9ee25fc172e29f47488486
InkoHX <me@inkohx.dev>
2022-12-27 02:24:49 +0900

    :art: format code

--- src/contentScript.js
+++ src/contentScript.js
@@ -4,27 +4,27 @@
 const hideAnalytics = () => {
   // TODO: インプレッションの子孫要素を削除しており表示が崩れているので、一番外側のタグ自体を消したい
   const analyticses = document.querySelectorAll("a[href$='/analytics']");
-  analyticses.forEach(analytics => analytics.parentNode?.remove());
-}
+  analyticses.forEach((analytics) => analytics.parentNode?.remove());
+};
 
 // ロード完了時のイベントハンドラ
 const onLoaded = () => {
-  const intervalId = setInterval(function() {
+  const intervalId = setInterval(function () {
     // mainタグがロードされたらintervalを終了する
     if (document.querySelector("a[href$='/analytics']") != null) {
       clearInterval(intervalId);
-      hideAnalytics()
+      hideAnalytics();
 
       const target = document.getElementsByTagName('section')[0];
       // オブザーバーの作成
       const observer = new MutationObserver(hideAnalytics);
-      const config = {attributes: true, childList: true, subtree: true};
+      const config = { attributes: true, childList: true, subtree: true };
       // 監視の開始
       observer.observe(target, config);
     } else {
-      console.log("waiting...")
+      console.log('waiting...');
     }
   }, 1000);
-}
+};
 
-window.addEventListener('load', onLoaded, false); 
+window.addEventListener('load', onLoaded, false);
